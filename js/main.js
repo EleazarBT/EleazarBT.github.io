1e3a8f23610c5337a53feaa1cc1bc1e89cbec232
EleazarBT <ebracoterry@gmail.com>
2022-03-18 07:07:06 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,17 +3,6 @@ function getInputValue(inputPhoneNumber) {
   if (inputPhoneNumber.value.match(phone)) {
     //document.querySelector('.numberClear').textContent = inputPhoneNumber.value.replace(/\s|-|/g, '');
     var number = inputPhoneNumber.value.replace(/\s|-|/g, "");
-    if (number == 938253432) {
-      alert("Hola dani, te extraño y tq mucho");
-    } else if (number == 923484250) {
-      alert("Holi Vale, tq mejor amiga");
-    } else if (number == 926711737 || number == 5898200) {
-      alert("Holi bb, tq mucho muchote eres la mejor ammy bb");
-    } else if (number == 928089435) {
-      alert("Holi bb greissy, tq mucho pero le pertenes a Armando");
-    } else if (number == 942724056) {
-      alert("Hola Romi, q tal? Espero no estes enojada");
-    }
     generatelink(number, getOption(), getMessage());
   } else {
     alert("No es un numero válido");
